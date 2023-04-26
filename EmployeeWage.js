e8f5ec5acadb9a90c191642fa99cab29b5df6b85
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-04-26 16:37:32 +0530

    [Ganesh] Add:Calculating of daily wage of an employee is added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,8 +1,20 @@
 console.log("Welcome to the Employee Wage Problem using JS");
 
-const IS_PRESENT=1;
-var empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==IS_PRESENT)
-console.log("Employee is present");
-else
-console.log("Employee is absent");
+const IS_PRESENT = 1;
+const IS_FULL_TIME=1;
+const IS_PART_TIME=2;
+const FULL_TIME_HRS=8;
+const PART_TIME_HRS=4;
+const WAGE_PER_HR=20;
+var empHrs=0;
+var empCheck = Math.floor(Math.random() * 10) % 3;
+switch(empCheck){
+    case IS_FULL_TIME:
+        empHrs=FULL_TIME_HRS;
+        break;
+    case IS_PART_TIME:
+        empHrs=PART_TIME_HRS;
+        break;
+}
+var dailyWage=WAGE_PER_HR*empHrs;
+console.log("Dailyt wage of an Employee is: "+dailyWage);
