8d5cd0bf37d837402f15c3e0012505cbd8b36590
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-04-26 16:55:17 +0530

    [Ganesh] Add:Employee Monthly wage with max hrs code is added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -7,6 +7,7 @@ const FULL_TIME_HRS=8;
 const PART_TIME_HRS=4;
 const WAGE_PER_HR=20;
 const NO_OF_DAYS=20;
+const MAX_HRS=100;
 function workingHours(empCheck) {
     switch (empCheck) {
         case IS_FULL_TIME:
@@ -20,9 +21,10 @@ function workingHours(empCheck) {
     }
 }
 var empHrs=0;
-for(var day=1;day<=NO_OF_DAYS;day++){
+for(var day=1;day<=NO_OF_DAYS&&empHrs<MAX_HRS;day++){
     var empCheck=Math.floor(Math.random()*10)%3;
     empHrs+=workingHours(empCheck)
 }
 var wage=WAGE_PER_HR*empHrs;
+console.log("No of hours an employee worked in a month is: "+empHrs);
 console.log("Monthly wage of an Employee is: "+wage);
