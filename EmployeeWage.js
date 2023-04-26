d44a35db7dd92119def43022550f9360ba2c0a90
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-04-26 16:31:00 +0530

    [Ganesh] Add:Employee attandance for day is added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,8 @@
 console.log("Welcome to the Employee Wage Problem using JS");
+
+const IS_PRESENT=1;
+var empCheck=Math.floor(Math.random()*10)%2;
+if(empCheck==IS_PRESENT)
+console.log("Employee is present");
+else
+console.log("Employee is absent");
